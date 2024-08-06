48252b250a81399f0df914af6e0fc4ed5d0b5a71
XCOD33 <denistraju155@gmail.com>
2024-08-06 14:22:44 +0700

    fix(dependencies): bump bodyParser to latest version
    This commit bumps bodyParser to the latest version.
    
    BREAKING CHANGE: bodyParser is no longer supported in Express 5.0.0 and above. This change updates bodyParser to the latest version, which is compatible with Express 5.0.0 and above.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParses = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const githubRoutes = require('./routes/github');
 const cors = require('cors');
