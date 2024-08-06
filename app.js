1af6b35662f2f1fc6619bbc94f39209dc274d8a7
XCOD33 <denistraju155@gmail.com>
2024-08-06 14:14:18 +0700

    fix: Update bodyparser dependency to 1.19.0
    Update bodyparser to 1.19.0 to fix a security vulnerability.
    
    BREAKING CHANGE: The minimum supported Node.js version for bodyparser is now 10.13.0.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParses.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
